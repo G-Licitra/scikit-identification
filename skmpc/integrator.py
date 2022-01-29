99d397c4d93ee9d7022d4aa0a7f8af14d6023e2c
G-Licitra <gianni.licitra7@gmail.com>
2022-01-29 22:44:22 +0100

    add simulation methor to rk4

---
+++ skmpc/integrator.py
@@ -0,0 +1,133 @@
+#%%
+import casadi as ca
+from model_selection import DynamicModel
+
+class RungeKutta4:
+    r"""Create Explicit Runge Kutta order 4 integrator`
+
+    Parameters
+    ---------
+    ode : casadi.Function
+        Function which define the model via ODE, formally, f: (x, u, theta) -> rhs. rhs: right-hand side, x: differential states, u: Input, theta: Model Parameters
+    N_steps_per_sample: int
+        Steps forward within one integration step
+    fs: int
+        frequency at which the system is sampled
+    N_steps: int
+        number of integration steps
+
+    Returns
+    ---------
+    one_sample : casadi.Function
+        One step forward model integrator
+    all_sample : casadi.Function
+        N steps forward model integrator
+    
+    """    
+    def __init__(self, model:DynamicModel, fs:int, N_steps_per_sample:int=1):
+    
+        dt = 1/fs/N_steps_per_sample
+        
+        # unpack model
+        x = model.states
+        u = model.inputs 
+        param = model.param
+        
+        # Function rhs, ordinary differential equation
+        Fode = model.Fmodel
+        
+        if param is None:
+            # Build an RK4 integrator with param as symbolic variable 
+            (k1, _) = Fode(x,u)
+            (k2, _) = Fode(x+dt/2.0*k1, u)
+            (k3, _) = Fode(x+dt/2.0*k2, u)
+            (k4, _) = Fode(x+dt*k3, u)
+            xf = x+dt/6.0*(k1+2*k2+2*k3+k4)
+
+            # Create a function that simulates one step propagation in a sample
+            one_step = ca.Function('one_step',[x, u],[xf])
+
+            # Carry out forward simulation within the entire sample time
+            X = x
+            for i in range(N_steps_per_sample):
+                X = one_step(X, u)
+
+            # Create a function that simulates all step propagation on a sample
+            self.one_sample = ca.Function('one_sample',[x, u], [X])
+        else:    
+            # Build an RK4 integrator with param as symbolic variable 
+            (k1, _) = Fode(x,u, param)
+            (k2, _) = Fode(x+dt/2.0*k1, u, param)
+            (k3, _) = Fode(x+dt/2.0*k2, u, param)
+            (k4, _) = Fode(x+dt*k3, u, param)
+            xf = x+dt/6.0*(k1+2*k2+2*k3+k4)
+
+            # Create a function that simulates one step propagation in a sample
+            one_step = ca.Function('one_step',[x, u, param],[xf])
+
+            # Carry out forward simulation within the entire sample time
+            X = x
+            for i in range(N_steps_per_sample):
+                X = one_step(X, u, param)
+
+            # Create a function that simulates all step propagation on a sample
+            self.one_sample = ca.Function('one_sample',[x, u, param], [X])
+
+    def simulate(self, x0, input, param=None):
+
+            # derive N_steps
+            N_steps = len(input)
+            
+            # Propagate simulation for N steps and generate trajectory
+            all_samples = self.one_sample.mapaccum("all_samples", N_steps)
+            x_sim =  np.array(all_samples(x0, input.values.T, ca.repmat(param, 1, N))).T
+            df_sim = pd.DataFrame(data=x_sim)             
+            return df_sim
+
+
+
+#%%----------------------------------------------------------------
+
+import numpy as np
+import pandas as pd
+from scipy.signal import chirp
+from model_selection import generate_model_parameters, DynamicModel
+import seaborn as sns
+
+# Choose an excitation signal
+np.random.seed(42)
+N = 2000  # Number of samples
+fs = 500 # Sampling frequency [hz]
+t = np.linspace(0, (N-1)*(1/fs), N)
+df_input = pd.DataFrame(data={'u1': 2*chirp(t, f0=1, f1=10, t1=5, method='logarithmic'),
+                              'u2': 2*np.random.random(N),
+                              },
+                        index = t)
+
+x0       = ca.DM([1,-1])    # Initial Condition x0 = [0;0]; [nx = 2]
+
+#%%----------------------------------------------------------------
+
+(x,u, param) = generate_model_parameters(nx=2, nu=2, nparam=2)
+
+# assign specific name
+x1, x2 = x[0], x[1]
+u1, u2 = u[0], u[1]
+ka, kb = param[0], param[1]
+
+param_truth = ca.DM([0.1, 0.5])
+
+# xdot = f(x,u,p) <==> rhs = f(x,u,p)
+rhs = [u1-ka*x1, 
+    u1*u2/x1-u1*x2/x1-kb*x2]
+
+sys = DynamicModel(states=x, inputs=u, param=param, model_dynamics=rhs)
+
+#%%
+rk4 = RungeKutta4(model=sys, fs=fs)
+
+
+#%%
+xsim = rk4.simulate(x0=x0, input=df_input, param=param_truth)
+
+# %%
--- skmpc/model_selection.py
+++ skmpc/model_selection.py
@@ -1,4 +1,3 @@
-#%%
 import pandas as pd
 import casadi as ca
 from typing import Union, Any
@@ -19,10 +18,8 @@ def generate_model_parameters(nx:int, nu:int, nparam:Union[str, None] = None):
     param = None if nparam == None else ca.MX.sym("param", nparam) # model parameters
     return (x, u, param)
 
-#%%
 
-
-class System:
+class DynamicModel:
     """Casadi Class System
     Formulation:
     \dot(x(t)) = f(x(x), u(t), \theta)
@@ -63,7 +60,7 @@ class System:
     def __init__(self, 
                  states, 
                  inputs, 
-                 parameters=None, 
+                 param=None, 
                  model_dynamics=None, 
                  output=None, 
                  name_state=None, 
@@ -73,12 +70,12 @@ class System:
         """Constructor. It runs every instance is created"""
         self.states = states
         self.inputs = inputs
-        self.parameters = parameters
+        self.param = param
      
         # get dimentions 
         self.nx = states.shape[0] # differential states
         self.nu = inputs.shape[0] # control input
-        self.np = -1 if parameters == None else parameters.shape[0] # model parameters
+        self.np = None if param == None else param.shape[0] # model parameters
            
         self.model_dynamics = ca.vcat(model_dynamics)
         
@@ -89,14 +86,14 @@ class System:
         # Construct Model
         # Form an ode function
         print(self.np) 
-        if self.np == -1:    
-            self.model = ca.Function('model',
+        if self.np == None:    
+            self.Fmodel = ca.Function('model',
                                     [self.states, self.inputs],
                                     [self.model_dynamics, self.output],
                                     ['x(y)','u(t)'], ['xdot(t) = f(x(t),u(t))', 'y(t) = g(x(t))'] )
         else:
-            self.model = ca.Function('model',
-                                    [self.states, self.inputs, self.parameters],
+            self.Fmodel = ca.Function('model',
+                                    [self.states, self.inputs, self.param],
                                     [self.model_dynamics, self.output],
                                     ['x(y)','u(t)','theta'], ['xdot(t) = f(x(t),u(t),theta)', 'y(t) = g(x(t))'] )
         
@@ -113,8 +110,9 @@ class System:
     def print_output(self):
         print(self.output)    
 
-    def evaluate(self, x0, u0, theta):
-        (rhs_num, y_num) = self.model(x0, u0, theta)    
+    def evaluate(self, x0, u0, param):
+        # TODO: add case with not param
+        (rhs_num, y_num) = self.model(x0, u0, param)    
         return (rhs_num, y_num)
           
     def __repr__(self):
@@ -122,45 +120,37 @@ class System:
         Dynamic Model with:
         - states:{self.nx} = [x1, x2]
         - inputs:{self.nu} = [u1, u2]
-        - parameter:{self.parameters} = [p1,p2]  
+        - parameter:{self.param} = [p1,p2]  
         """
         
         return "self.model.print_dimensions()"  # string to print
 
+if __name__ == "__main__":  # when run for testing only
 
-#%%
-#if __name__ == "__main__":  # when run for testing only
+    (x,u, param) = generate_model_parameters(nx=2, nu=2, nparam=2)
 
-(x,u, param) = generate_model_parameters(nx=2, nu=2, nparam=2)
+    # assign specific name
+    x1, x2 = x[0], x[1]
+    u1, u2 = u[0], u[1]
+    ka, kb = param[0], param[1]
 
-# assign specific name
-x1, x2 = x[0], x[1]
-u1, u2 = u[0], u[1]
-ka, kb = param[0], param[1]
+    # xdot = f(x,u,p) <==> rhs = f(x,u,p)
+    rhs = [u1-ka*x1, 
+        u1*u2/x1-u1*x2/x1-kb*x2]
 
-# xdot = f(x,u,p) <==> rhs = f(x,u,p)
-rhs = [u1-ka*x1, 
-       u1*u2/x1-u1*x2/x1-kb*x2]
+    #%%
+    sys = DynamicModel(states=x, inputs=u, param=param, model_dynamics=rhs)
+    sys.print_dimensions()
+    sys.print_ode()
 
-#%%
-sys = System(states=x, inputs=u, parameters=param, model_dynamics=rhs)
-sys.print_dimensions()
+    # numerical evaluation ===================================================
+    x_test     = [0.1,-0.1]
+    u_test     = [0.2,-0.1]
+    theta_test = [0.1,0.5]
+    rhs_num, y_num  = sys.evaluate(x_test,u_test,theta_test)
 
-# %%
-sys.print_ode()
-# %%
-# numerical evaluation ===================================================
-x_test     = [0.1,-0.1]
-u_test     = [0.2,-0.1]
-theta_test = [0.1,0.5]
-rhs_num, y_num  = sys.evaluate(x_test,u_test,theta_test)
+    print(f"rhs = {rhs_num}, \ny = {y_num}") 
 
-print(f"rhs = {rhs_num}, \ny = {y_num}") 
+    # rhs = [0.19, 0.05], 
+    # y = [0.1, -0.1]
 
-# rhs = [0.19, 0.05], 
-# y = [0.1, -0.1]
-
-
-# %%
-
-# %%
