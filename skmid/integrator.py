ab9d0253ffc8c903a6a8770233a5d3a8ec237fd3
G-Licitra <gianni.licitra7@gmail.com>
2022-04-02 21:25:20 +0200

    [pre-commit] handle inputs function

--- skmid/integrator.py
+++ skmid/integrator.py
@@ -1,4 +1,6 @@
 #%%
+import warnings
+
 import casadi as ca
 import numpy as np
 
@@ -6,7 +8,7 @@ from skmid.models import _infer_model_type
 from skmid.models import DynamicModel
 
 
-class RungeKutta4(DynamicModel):
+class RungeKutta4:
     r"""Create Explicit Runge Kutta order 4 integrator`
 
     Parameters
@@ -39,25 +41,18 @@ class RungeKutta4(DynamicModel):
 
         __N_steps_per_sample: int = 1
 
-        dt = 1 / fs / __N_steps_per_sample
-
-        # unpack model
-        x = model.states
-        u = model.inputs
-        param = model.param
-
-        # Function rhs, ordinary differential equation
-        Fode = model.Fmodel
-
-        self.__model_type = _infer_model_type(model)
+        __dt = 1 / fs / __N_steps_per_sample
+        self.fs = fs  # frequency sample
+        self.model = model
+        self.__model_type = _infer_model_type(self.model)
 
         if self.__model_type["struct"] == "f(x,u)":
             # Build an RK4 integrator with param as symbolic variable
-            (k1, _) = Fode(x, u)
-            (k2, _) = Fode(x + dt / 2.0 * k1, u)
-            (k3, _) = Fode(x + dt / 2.0 * k2, u)
-            (k4, _) = Fode(x + dt * k3, u)
-            xf = x + dt / 6.0 * (k1 + 2 * k2 + 2 * k3 + k4)
+            (k1, _) = self.model.Fmodel(x, u)
+            (k2, _) = self.model.Fmodel(x + __dt / 2.0 * k1, u)
+            (k3, _) = self.model.Fmodel(x + __dt / 2.0 * k2, u)
+            (k4, _) = self.model.Fmodel(x + __dt * k3, u)
+            xf = x + __dt / 6.0 * (k1 + 2 * k2 + 2 * k3 + k4)
 
             # Create a function that simulates one step propagation in a sample
             one_step = ca.Function("one_step", [x, u], [xf])
@@ -73,16 +68,16 @@ class RungeKutta4(DynamicModel):
                 [x, u],
                 [X],
                 ["x[k]", "u[k]"],
-                ["x[k+1] = x[k] + dt * f(x[k], u[k])"],
+                ["x[k+1] = x[k] + __dt * f(x[k], u[k])"],
             )
 
         elif self.__model_type["struct"] == "f(x)":
             # Build an RK4 integrator with param as symbolic variable
-            (k1, _) = Fode(x)
-            (k2, _) = Fode(x + dt / 2.0 * k1)
-            (k3, _) = Fode(x + dt / 2.0 * k2)
-            (k4, _) = Fode(x + dt * k3)
-            xf = x + dt / 6.0 * (k1 + 2 * k2 + 2 * k3 + k4)
+            (k1, _) = self.model.Fmodel(x)
+            (k2, _) = self.model.Fmodel(x + __dt / 2.0 * k1)
+            (k3, _) = self.model.Fmodel(x + __dt / 2.0 * k2)
+            (k4, _) = self.model.Fmodel(x + __dt * k3)
+            xf = x + __dt / 6.0 * (k1 + 2 * k2 + 2 * k3 + k4)
 
             # Create a function that simulates one step propagation in a sample
             one_step = ca.Function("one_step", [x], [xf])
@@ -94,16 +89,16 @@ class RungeKutta4(DynamicModel):
 
             # Create a function that simulates all step propagation on a sample
             self.one_sample = ca.Function(
-                "one_sample", [x], [X], ["x[k]"], ["x[k+1] = x[k] + dt * f(x[k])"]
+                "one_sample", [x], [X], ["x[k]"], ["x[k+1] = x[k] + __dt * f(x[k])"]
             )
 
         elif self.__model_type["struct"] == "f(x,p)":
             # Build an RK4 integrator with param as symbolic variable
-            (k1, _) = Fode(x, param)
-            (k2, _) = Fode(x + dt / 2.0 * k1, param)
-            (k3, _) = Fode(x + dt / 2.0 * k2, param)
-            (k4, _) = Fode(x + dt * k3, param)
-            xf = x + dt / 6.0 * (k1 + 2 * k2 + 2 * k3 + k4)
+            (k1, _) = self.model.Fmodel(x, param)
+            (k2, _) = self.model.Fmodel(x + __dt / 2.0 * k1, param)
+            (k3, _) = self.model.Fmodel(x + __dt / 2.0 * k2, param)
+            (k4, _) = self.model.Fmodel(x + __dt * k3, param)
+            xf = x + __dt / 6.0 * (k1 + 2 * k2 + 2 * k3 + k4)
 
             # Create a function that simulates one step propagation in a sample
             one_step = ca.Function("one_step", [x, param], [xf])
@@ -119,16 +114,16 @@ class RungeKutta4(DynamicModel):
                 [x, param],
                 [X],
                 ["x[k]", "param"],
-                ["x[k+1] = x[k] + dt * f(x[k], param)"],
+                ["x[k+1] = x[k] + __dt * f(x[k], param)"],
             )
 
         elif self.__model_type["struct"] == "f(x,u,p)":
             # Build an RK4 integrator with param as symbolic variable
-            (k1, _) = Fode(x, u, param)
-            (k2, _) = Fode(x + dt / 2.0 * k1, u, param)
-            (k3, _) = Fode(x + dt / 2.0 * k2, u, param)
-            (k4, _) = Fode(x + dt * k3, u, param)
-            xf = x + dt / 6.0 * (k1 + 2 * k2 + 2 * k3 + k4)
+            (k1, _) = self.model.Fmodel(x, u, param)
+            (k2, _) = self.model.Fmodel(x + __dt / 2.0 * k1, u, param)
+            (k3, _) = self.model.Fmodel(x + __dt / 2.0 * k2, u, param)
+            (k4, _) = self.model.Fmodel(x + __dt * k3, u, param)
+            xf = x + __dt / 6.0 * (k1 + 2 * k2 + 2 * k3 + k4)
 
             # Create a function that simulates one step propagation in a sample
             one_step = ca.Function("one_step", [x, u, param], [xf])
@@ -144,43 +139,93 @@ class RungeKutta4(DynamicModel):
                 [x, u, param],
                 [X],
                 ["x[k]", "u[k]", "param"],
-                ["x[k+1] = x[k] + dt * f(x[k], u[k], param)"],
+                ["x[k+1] = x[k] + __dt * f(x[k], u[k], param)"],
             )
         else:
             raise ValueError("Model type not supported")
 
     def simulate(self, x0, input=None, param=None):
 
-        # TODO input flexible for pd and np,
-        # derive N_steps.
-        N_steps = len(input)
+        input = self.__validate_input(input)
 
-        # get time and attach one extra sample to time
-        time = input.index
-        ts = time[-1] + (time[1] - time[0])  # sample time
-        time = np.concatenate([time.values.reshape(-1, 1), np.array(ts, ndmin=2)])
+        # TODO input flexible for pd and np,  nd
+        # derive N_steps.
+        # N_steps = len(input)
+
+        # # get time and attach one extra sample to time
+        # time = input.index
+        # ts = time[-1] + (time[1] - time[0])  # sample time
+        # time = np.concatenate([time.values.reshape(-1, 1), np.array(ts, ndmin=2)])
 
         # Propagate simulation for N steps and generate trajectory
-        all_samples = self.one_sample.mapaccum("all_samples", N_steps)
+        all_samples = self.one_sample.mapaccum("all_samples", self.N_steps)
 
         if self.__model_type["struct"] == "f(x,u)":
             x_sim = np.array(all_samples(x0, input.values.T))
         elif self.__model_type["struct"] == "f(x)":
             x_sim = np.array(all_samples(x0))
         elif self.__model_type["struct"] == "f(x,p)":
-            x_sim = np.array(all_samples(x0, ca.repmat(param, 1, N_steps)))
+            x_sim = np.array(all_samples(x0, ca.repmat(param, 1, self.N_steps)))
         elif self.__model_type["struct"] == "f(x,u,p)":
             x_sim = np.array(
-                all_samples(x0, input.values.T, ca.repmat(param, 1, N_steps))
+                all_samples(x0, input.values.T, ca.repmat(param, 1, self.N_steps))
             )
 
-        # attach the initial condition to x_sim with dim=[N_steps+1, nx]
-        x_sim = np.concatenate([np.array(x0).reshape(1, -1), x_sim.T])
+        # pack differential state x attaching initial condition x0
+        self.x_sim_ = pd.DataFrame(
+            data=np.concatenate([np.array(x0).reshape(1, -1), x_sim.T]),
+            index=input.index.append(pd.Index([input.index[-1] + 1 / self.fs])),
+            columns=self.model.state_name,
+        )
 
-        df_sim = pd.DataFrame(data=x_sim, index=time.reshape(-1))
+    def __validate_input(self, input):
+        """determine the type of input"""
 
-        # TODO get X, Y
-        return df_sim
+        # check dimension of input and param
+        self.N_steps = len(input)
+
+        # get time and attach one extra sample to time
+        if isinstance(input, np.ndarray):
+            # convert input to pandas dataframe
+            input = pd.DataFrame(
+                data=input,
+                index=np.linspace(0, (self.N_steps - 1) * (1 / self.fs), self.N_steps),
+            )
+
+        if isinstance(input, pd.DataFrame):  # check if input is a dataframe
+
+            if input.shape[1] == 1:
+                # Check if input is a series
+                input = input.to_frame()
+
+            if np.mean(np.diff(input.index)) != 1 / self.fs:
+                # Detect sample frequency mismatch
+                input.index = np.linspace(
+                    0, (self.N_steps - 1) * (1 / self.fs), self.N_steps
+                )
+                warnings.warn(
+                    f"""The input index has a different fs than specified in the object.
+                                  The input index has been modified by using fs={self.fs} Hz.
+                                """
+                )
+
+            if not (set(input.columns) == set(self.model.input_name)):
+                # Detect input name mismatch
+                warnings.warn(
+                    f"""The input names is not consistent with the model input.
+                                        the simulation has considered the input as if they were {self.model.input_name}.
+                                     """
+                )
+                input.columns = self.model.input_name
+
+            return input
+
+        input = pd.DataFrame(
+            data=input.values,
+            index=np.linspace(0, (self.N_steps - 1) * (1 / self.fs), self.N_steps),
+        )
+
+        return input
 
     def plot(self):
         return -1
@@ -209,7 +254,7 @@ df_input = pd.DataFrame(
 
 x0 = [1, -1]  # Initial Condition x0 = [0;0]; [nx = 2]
 
-# #%%----------------------------------------------------------------
+#%%----------------------------------------------------------------
 
 (x, u, param) = generate_model_parameters(nx=2, nu=2, nparam=2)
 
@@ -229,8 +274,12 @@ sys = DynamicModel(states=x, inputs=u, param=param, model_dynamics=rhs)
 rk4 = RungeKutta4(model=sys, fs=fs)
 
 # #%%
-xsim = rk4.simulate(x0=x0, input=df_input, param=param_truth)
+rk4.simulate(x0=x0, input=df_input, param=param_truth)
+
+rk4.x_sim_
 
 # # %%
 print("Done!")
 # xsim.plot()
+
+# %%
