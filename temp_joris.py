0d9c8b1343fc0d1ece0c8ab041182164d87dba8f
G-Licitra <gianni.licitra7@gmail.com>
2022-04-12 22:50:27 +0200

    [pre-commit] nest DynamicModel in exampke

--- temp_joris.py
+++ temp_joris.py
@@ -1,18 +1,10 @@
 import casadi as ca
+import matplotlib.pyplot as plt
 import numpy as np
 
-
-# In this example, we fit a nonlinear model to measurements
-#
-# This example uses more advanced constructs than the vdp* examples:
-# Since the number of control intervals is potentially very large here,
-# we use memory-efficient Map and MapAccum, in combination with
-# codegeneration.
-#
-# We will be working with a 2-norm objective:
-# || y_measured - y_simulated ||_2^2
-#
-# This form is well-suited for the Gauss-Newton Hessian approximation.
+from skmid.integrator import RungeKutta4
+from skmid.models import DynamicModel
+from skmid.models import generate_model_parameters
 
 ############ SETTINGS #####################
 N = 10000  # Number of samples
@@ -23,47 +15,70 @@ param_guess = ca.DM([5, 2, 1, 5])
 scale = ca.vertcat(1e-6, 1e-4, 1, 1)
 
 ############ MODELING #####################
-y = ca.MX.sym("y")
-dy = ca.MX.sym("dy")
-u = ca.MX.sym("u")
 
-states = ca.vertcat(y, dy)
-controls = u
+(state, input, param) = generate_model_parameters(nstate=2, ninput=1, nparam=4)
 
-M = ca.MX.sym("x")
-c = ca.MX.sym("c")
-k = ca.MX.sym("k")
-k_NL = ca.MX.sym("k_NL")
 
-params = ca.vertcat(M, c, k, k_NL)
+# rhs = [-alpha * x[0] + p[0] * x[0] * x[1], gamma * x[1] - p[1] * x[0] * x[1
+#                                                                           ]]
 
-rhs = ca.vertcat(dy, (u - k_NL * y**3 - k * y - c * dy) / M)
+
+y, dy = state[0], state[1]
+
+u = input[0]
+
+M, c, k, k_NL = param[0], param[1], param[2], param[3]
+
+# M = ca.MX.sym("x")
+# c = ca.MX.sym("c")
+# k = ca.MX.sym("k")
+# k_NL = ca.MX.sym("k_NL")
+
+# params = ca.vertcat(M, c, k, k_NL)
+
+rhs = [dy, (u - k_NL * y**3 - k * y - c * dy) / M]
+
+
+model = DynamicModel(
+    states=state,
+    inputs=input,
+    param=param,
+    model_dynamics=rhs,
+)
+
+model.print_summary()
+
+rhs_num, y_num = model.evaluate(
+    state_num=[1.0, 1.0], input_num=0, param_num=param_truth
+)
+print(f"rhs = {rhs_num}, \ny = {y_num}")
+
 
 # Form an ode function
-ode = ca.Function("ode", [states, controls, params], [rhs])
+# ode = ca.Function("ode", [states, controls, params], [rhs])
 
 ############ Creating a simulator ##########
 N_steps_per_sample = 10
 dt = 1 / fs / N_steps_per_sample
 
 # Build an integrator for this system: Runge Kutta 4 integrator
-k1 = ode(states, controls, params)
-k2 = ode(states + dt / 2.0 * k1, controls, params)
-k3 = ode(states + dt / 2.0 * k2, controls, params)
-k4 = ode(states + dt * k3, controls, params)
+k1, _ = model.model_function(state, input, param)
+k2, _ = model.model_function(state + dt / 2.0 * k1, input, param)
+k3, _ = model.model_function(state + dt / 2.0 * k2, input, param)
+k4, _ = model.model_function(state + dt * k3, input, param)
 
-states_final = states + dt / 6.0 * (k1 + 2 * k2 + 2 * k3 + k4)
+states_final = state + dt / 6.0 * (k1 + 2 * k2 + 2 * k3 + k4)
 
 # Create a function that simulates one step propagation in a sample
-one_step = ca.Function("one_step", [states, controls, params], [states_final])
+one_step = ca.Function("one_step", [state, input, param], [states_final])
 
-X = states
+X = state
 
 for i in range(N_steps_per_sample):
-    X = one_step(X, controls, params)
+    X = one_step(X, input, param)
 
 # Create a function that simulates all step propagation on a sample
-one_sample = ca.Function("one_sample", [states, controls, params], [X])
+one_sample = ca.Function("one_sample", [state, input, param], [X])
 
 ############ Simulating the system ##########
 all_samples = one_sample.mapaccum("all_samples", N)
@@ -114,12 +129,12 @@ def gauss_newton(e, nlp, V):
 
 # Note, it is in general a good idea to scale your decision variables such
 # that they are in the order of ~0.1..100
-X_symbolic = all_samples(x0, u_data, ca.repmat(params * scale, 1, N))
+X_symbolic = all_samples(x0, u_data, ca.repmat(param * scale, 1, N))
 
 e = y_data - X_symbolic[0, :].T
-nlp = {"x": params, "f": 0.5 * ca.dot(e, e)}
+nlp = {"x": param, "f": 0.5 * ca.dot(e, e)}
 
-solver = gauss_newton(e, nlp, params)
+solver = gauss_newton(e, nlp, param)
 
 sol = solver(x0=param_guess)
 
@@ -133,13 +148,13 @@ assert ca.norm_inf(sol["x"] * scale - param_truth) < 1e-8
 # All states become decision variables
 X = ca.MX.sym("X", 2, N)
 
-Xn = one_sample.map(N, "openmp")(X, u_data.T, ca.repmat(params * scale, 1, N))
+Xn = one_sample.map(N, "openmp")(X, u_data.T, ca.repmat(param * scale, 1, N))
 
 gaps = Xn[:, :-1] - X[:, 1:]
 
 e = y_data - Xn[0, :].T
 
-V = ca.veccat(params, X)
+V = ca.veccat(param, X)
 
 nlp = {"x": V, "f": 0.5 * ca.dot(e, e), "g": ca.vec(gaps)}
 
